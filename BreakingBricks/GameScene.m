70a7c10402e43580dd00b1aa4004ba02cd01065b
Anatoly Kovalev <inatoley@gmail.com>
2015-07-18 21:00:35 +0300

    playing with paddle

--- BreakingBricks/GameScene.m
+++ BreakingBricks/GameScene.m
@@ -8,7 +8,26 @@
 
 #import "GameScene.h"
 
+@interface GameScene ()
+
+@property (nonatomic) SKSpriteNode *paddle;
+
+@end
+
 @implementation GameScene
+
+- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        CGPoint newPosition = CGPointMake(location.x, 100);
+        if (location.x <= self.paddle.size.width/2) {
+            self.paddle.position = CGPointMake(self.paddle.size.width/2, 100);
+        } else if (location.x >= self.frame.size.width - self.paddle.size.width/2) {
+            self.paddle.position = CGPointMake(self.frame.size.width - self.paddle.size.width/2, 100);
+        } else self.paddle.position = newPosition;
+    }
+}
+
 
 - (void)addBall {
     // Setting up the ball
@@ -25,9 +44,22 @@
     [self addChild:ball];
     
     // Creating vector
-    CGVector myVector = CGVectorMake(50.0, 20.0);
+    CGVector myVector = CGVectorMake(20.0, 10.0);
     // Applying vector to physicsBody
     [ball.physicsBody applyImpulse:myVector];
+}
+
+-(void) addPlayer {
+    // create a paddle sprite
+    self.paddle = [SKSpriteNode spriteNodeWithImageNamed:@"paddle"];
+    // positioning it
+    self.paddle.position = CGPointMake(self.frame.size.width/2, 100);
+    // add a physics body
+    self.paddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddle.frame.size];
+    // make it static
+    self.paddle.physicsBody.dynamic = NO;
+    // add to the scene
+    [self addChild:self.paddle];
 }
 
 -(void)didMoveToView:(SKView *)view {
@@ -43,11 +75,11 @@
     
     
     // Changing gravity settings
-    self.physicsWorld.gravity = CGVectorMake(0.0, -5.0);
+    self.physicsWorld.gravity = CGVectorMake(0.0, -4.0);
     
     
     [self addBall];
-    
+    [self addPlayer];
 }
 
 -(void)update:(CFTimeInterval)currentTime {
