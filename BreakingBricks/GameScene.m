da3c30b3372d6f9209fba07e0796f648ce3e8a72
Anatoly Kovalev <inatoley@gmail.com>
2015-07-23 17:44:28 +0300

    detecting contacts and stuff

--- BreakingBricks/GameScene.m
+++ BreakingBricks/GameScene.m
@@ -14,7 +14,27 @@
 
 @end
 
+static const uint32_t ballCategory  = 1; // 00000000000000000000000000000001
+static const uint32_t brickCategory = 2; // 00000000000000000000000000000010
+static const uint32_t paddleCategory = 4;// we wonna flip just one bit, no more, no less
+static const uint32_t edgeCategory = 8;  // 00000000000000000000000000001000
+
+/*
+ // these are effectively the same
+ static const uint32_t ballCategory = 0x1;
+ static const uint32_t brickCategory = 0x1 << 1;
+ static const uint32_t paddleCategory = 0x1 << 2;
+ static const uint32_t edgeCategory = 0x1 << 3;
+ */
+
+
+
 @implementation GameScene
+
+-(void)didBeginContact:(SKPhysicsContact *)contact {
+    NSLog(@"boing");
+}
+
 
 - (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     for (UITouch *touch in touches) {
@@ -35,6 +55,8 @@
         // adding a physics body
         brick.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:brick.frame.size];
         brick.physicsBody.dynamic = NO;
+        // Giving the brick its own category
+        brick.physicsBody.categoryBitMask = brickCategory;
         
         int xPos = self.frame.size.width/5 * (i+1);
         int yPos = self.frame.size.height - 50;
@@ -55,6 +77,14 @@
     ball.physicsBody.linearDamping = 0;
     // Bounciness of the physicsBody
     ball.physicsBody.restitution = 1;
+    // Giving the ball its own category
+    ball.physicsBody.categoryBitMask = ballCategory;
+    // What categories we are interested to receive notifications about contact
+    ball.physicsBody.contactTestBitMask = brickCategory | paddleCategory;
+    // CollisionMask are all on by default (0000 etc) by overriding its behaviour we are making it (0000 etc) so ball will interact only with categories named below
+    //ball.physicsBody.collisionBitMask = edgeCategory | paddleCategory;
+    
+    // Add the sprite node to the scene
     [self addChild:ball];
     
     // Creating vector
@@ -72,6 +102,9 @@
     self.paddle.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.paddle.frame.size];
     // make it static
     self.paddle.physicsBody.dynamic = NO;
+    // Giving the paddle its own category
+    self.paddle.physicsBody.categoryBitMask = paddleCategory;
+    
     // add to the scene
     [self addChild:self.paddle];
 }
@@ -82,6 +115,8 @@
     
     // Adding physics to the scene
     self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    // Giving the edge of the screen its own category
+    self.physicsBody.categoryBitMask = edgeCategory;
 
     
     NSLog(@"Width of the screen %f",self.size.width);
@@ -91,6 +126,8 @@
     // Changing gravity settings
     self.physicsWorld.gravity = CGVectorMake(0.0, -2.0);
     
+    // Setting the contactDelegate to listen contacts from .self
+    self.physicsWorld.contactDelegate = self;
     
     [self addBall];
     [self addPlayer];
